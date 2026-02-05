8d1d1f84855bca29132af2d6074de2dd488aa70d
Peter Akaliro <pin4eva@hotmail.com>
2026-02-05 22:40:32 +0100

    chore(migrations): remove hardcoded database connection string from migration scripts

--- scripts/inspect_email_token_migration.js
+++ scripts/inspect_email_token_migration.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 (async () => {
-  const connectionString = process.env.PRODUCTION_DATABASE_URL || 'postgres://0c3fb87ee4d3434062ad439345d84a680eda7d4b7aa0693c2a6c29ce1790796d:sk_RO6AA9GsEXx6W6jmyKYFo@db.prisma.io:5432/helar_dev?sslmode=require';
+  const connectionString = process.env.PRODUCTION_DATABASE_URL || '';
   const client = new Client({ connectionString });
   try {
     await client.connect();
