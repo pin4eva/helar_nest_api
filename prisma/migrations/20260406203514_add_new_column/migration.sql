f6cfe32b42db9fd1893973dd17853c4df2537cb7
Peter Akaliro <pin4eva@hotmail.com>
2026-04-08 13:19:08 +0100

    fix: update Prisma Client and Query Engine versions to 7.3.0 and 9d6ad21c respectively

--- prisma/migrations/20260406203514_add_new_column/migration.sql
+++ prisma/migrations/20260406203514_add_new_column/migration.sql
@@ -1,18 +1,17 @@
 /*
-  Warnings:
-
-  - A unique constraint covering the columns `[ref]` on the table `final_bar_exam_qa` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[slug]` on the table `final_bar_exam_qa` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `year` to the `final_bar_exam_qa` table without a default value. This is not possible if the table is not empty.
-
-*/
+ Warnings:
+ 
+ - A unique constraint covering the columns `[ref]` on the table `final_bar_exam_qa` will be added. If there are existing duplicate values, this will fail.
+ - A unique constraint covering the columns `[slug]` on the table `final_bar_exam_qa` will be added. If there are existing duplicate values, this will fail.
+ - Added the required column `year` to the `final_bar_exam_qa` table without a default value. This is not possible if the table is not empty.
+ 
+ */
 -- AlterTable
-ALTER TABLE "final_bar_exam_qa" ADD COLUMN     "ref" INTEGER,
-ADD COLUMN     "slug" TEXT,
-ADD COLUMN     "year" INTEGER NOT NULL;
-
+ALTER TABLE "final_bar_exam_qa"
+ADD COLUMN "ref" INTEGER,
+  ADD COLUMN "slug" TEXT,
+  ADD COLUMN "year" INTEGER;
 -- CreateIndex
 CREATE UNIQUE INDEX "final_bar_exam_qa_ref_key" ON "final_bar_exam_qa"("ref");
-
 -- CreateIndex
 CREATE UNIQUE INDEX "final_bar_exam_qa_slug_key" ON "final_bar_exam_qa"("slug");
