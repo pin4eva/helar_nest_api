6e6c484386177846643eb361f436fdec97119571
Peter Akaliro <pin4eva@hotmail.com>
2026-04-08 13:38:24 +0100

    fix: backfill NULL year values before setting NOT NULL constraint

--- prisma/migrations/20260408122757_default_year/migration.sql
+++ prisma/migrations/20260408122757_default_year/migration.sql
@@ -4,6 +4,8 @@
   - Made the column `year` on table `final_bar_exam_qa` required. This step will fail if there are existing NULL values in that column.
 
 */
+-- BackfillNulls
+UPDATE "final_bar_exam_qa" SET "year" = 1900 WHERE "year" IS NULL;
 -- AlterTable
 ALTER TABLE "final_bar_exam_qa" ALTER COLUMN "year" SET NOT NULL,
 ALTER COLUMN "year" SET DEFAULT 1900;
